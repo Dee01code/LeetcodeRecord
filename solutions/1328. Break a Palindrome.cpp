2e2f4e9c227e30b79c27b540b03d3a95d5f0e142
Dee01code <109283404+Dee01code@users.noreply.github.com>
2022-10-11 01:36:44 +0530

    7 ms

---
+++ solutions/1328. Break a Palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    public:
+        string breakPalindrome(string p)
+        {
+​
+            int n = p.size();
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (p[i] != 'a')
+                {
+                    p[i] = 'a';
+                    return p;
+                }
+            }
+            p[n - 1] = 'b';
+            return (n < 2) ? "" : p;
+        }
+};
